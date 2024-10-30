4543ce8d497ec20e7d76a96bca4fb9e2be6438a3
ZOIA1285 <zoiamyshkova@gmail.com>
2024-10-30 20:51:01 +0300

    Add files via upload

---
+++ modul_2_3.py
@@ -0,0 +1,15 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+a = 0
+while len(my_list) > 0 :
+    if my_list[a] >= 0:
+        if my_list[a] != 0:
+            print(my_list[a])
+            a += 1
+        else:
+            a += 1
+        continue
+    else:
+        break
+
+
+
